562f59d2fefb74fd0ebb51cfadc66a777be882da
saipriyaan <saipriyaan1@gmail.com>
2021-09-26 15:56:04 +0530

    first commit

---
+++ for_loop_example.py
@@ -0,0 +1,10 @@
+times=0
+j=1000
+for i in range(j):
+    n1= int(input("Enter the number 1 :"))
+    n2= int(input("Enter the number 2 :"))
+    
+    print("Process Number:",i)
+    print("The sum is :",n1+n2)                                       
+    i=+1
+    
